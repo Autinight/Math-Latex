27ef9fc2ee106ae6e62ab2743da5b3560d091efc
Autin <Autinight@outlook.com>
2025-09-18 15:55:29 +0800

    9-18

--- HW/LA/main.ex
+++ HW/LA/main.ex
@@ -1,3 +1,5 @@
-\contentsline {tcolorbox}{\numberline {1.0.1}}{1}{amzboxcounter.1.0.1}%
-\contentsline {tcolorbox}{\numberline {1.0.2}}{2}{amzboxcounter.1.0.2}%
-\contentsline {tcolorbox}{\numberline {1.0.3}}{3}{amzboxcounter.1.0.3}%
+\contentsline {tcolorbox}{\numberline {1.0.1}}{2}{amzboxcounter.1.0.1}%
+\contentsline {tcolorbox}{\numberline {1.0.2}}{3}{amzboxcounter.1.0.2}%
+\contentsline {tcolorbox}{\numberline {1.0.3}}{4}{amzboxcounter.1.0.3}%
+\contentsline {tcolorbox}{\numberline {1.0.4}}{8}{amzboxcounter.1.0.4}%
+\contentsline {tcolorbox}{\numberline {1.0.5}}{11}{amzboxcounter.1.0.5}%
